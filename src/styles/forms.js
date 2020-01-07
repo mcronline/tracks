679a7705b5612fd5702048e7b64eb6327effac77
Michel Rozatti <contact@michelcurti.dev>
2020-01-07 18:08:17 -0300

    forms stylesheet

---
+++ src/styles/forms.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+export const formStyle = StyleSheet.create({
+    input : {
+        marginVertical : 10
+    },
+    button : {
+        marginVertical : 10
+    }
+});
