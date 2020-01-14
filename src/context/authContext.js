2b39187b5eb85705890727deab4c08befe15e907
Michel Rozatti <contact@michelcurti.dev>
2020-01-14 09:42:31 -0300

    Implemented signup process

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,48 +1,59 @@
 import createDataContext from './createDataContext';
 import trackerApi from '../api/tracker';
+import ErrorAlert from '../components/ErrorAlert';
 
 const authReducer = (state, action) => {
-
+    console.log(action);
     switch(action.type){
+
+        case 'signup':
+            return {...state, token : action.payload.token};
+
         default:
             return state;
     }
 }
 
 const actions = {
-    'signin' : dispatch => {
-        return async ({ email, password}) => {
-            try{
-                const response = await trackerApi.get('/signin',{email,password});
-                const token = response.data.token;
-                dispatch({
-                    type : 'signin',
-                    payload : {
-                        token
-                    }
-                });
-            }catch(err){
-                
-            }
-        }
-    },
-    'signup' : dispatch => {
-        return ({ email, password}) => {
+    'signin' : dispatch => async ({ email, password }) => {
+        try{
+            const response = await trackerApi.get('/signin',{email,password});
+            const token = response.data.token;
             dispatch({
-                type : 'signup',
+                type : "signin",
                 payload : {
-                    email,
-                    password
+                    token
                 }
             });
+        }catch(err){
+            ErrorAlert(err.response.data, "Problem to Sign In");
         }
     },
-    'signout' : dispatch => {
-        return () => {
+    'signup' : dispatch => async ({ email, password }) => {
+        try{
+            const response = await trackerApi.post('/signup',{email,password});
+            console.log(response.data)                ;
+            const token = response.data.token;
+            //console.log(token);
             dispatch({
-                type : 'signout'
+                type : "signup",
+                payload : {
+                    token
+                }
             });
+        }catch(err){
+            console.log(err.response.data);
+            ErrorAlert(err.response.data.message, 'Problem to Sign Up');
         }
+    
+    },
+    'signout' : dispatch => () => {
+        dispatch({
+            type : "signout",
+            payload : {
+                token : null
+            }
+        });
     }
 };
 
