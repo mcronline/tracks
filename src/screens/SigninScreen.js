8184f2a438fcdd57a9ae17074d9a188197bc054c
Michel <contact@michelcurti.dev>
2020-01-16 11:50:23 -0300

    mplemented signin functionality

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import LoginForm from '../components/LoginForm';
+import { Context as authContext } from "../context/authContext";
 
 const SignupScreen = ({ navigation }) =>{
+
+    const {state, signin} = useContext(authContext);
 
     return(
         <View style={styles.container}>
             <Text style={{ marginVertical : 15, textAlign : 'center', fontSize : 36}}>Sign in for Tracks</Text>
+
             <LoginForm 
                 buttonLabel="Sign in"
-                buttonAction={() => {}}
+                buttonAction={(email, password) => signin({ email, password })}
              />
+
             <Text style={{ textAlign : 'center' }}>Don't have an account?</Text>
             <TouchableOpacity onPress={() => navigation.navigate('Signup')}>
                 <Text style={{ textAlign : 'center' }}>SIGN UP</Text>
