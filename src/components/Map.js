b78716516cebf772858af85b0aa07e8d7d825a8b
Michel <contact@michelcurti.dev>
2020-01-21 09:45:36 -0300

    set locations state and send to Maps with props

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,33 +2,12 @@ import React from 'react';
 import { Text, StyleSheet } from 'react-native';
 import MapView, { Polyline } from 'react-native-maps';
 
-const Map = () => {
-
-    const points = [];
-    const initialLatitude = 37.33233;
-    const initialLongitude = -122.03121;
-    const radius = 0.01;
-    let x;
-    let y;
-
-    function circleX (angle, radius){
-        return radius * Math.cos(angle);
-    }
-
-    function circleY (angle, radius){
-        return radius * Math.sin(angle);
-    }
-
-    for(let angle=0; angle < 360; angle++){
-        x = initialLatitude + circleX(angle, radius);
-        y = initialLongitude + circleY(angle, radius)
-        
-        points.push({
-            latitude : parseFloat(x.toFixed(5)),
-            longitude : parseFloat(y.toFixed(5))
-        })
-    }
-
+const Map = ( {
+    initialLatitude,
+    initialLongitude,
+    points
+} ) => {
+    console.log(points[points.length - 1]);
     return(
         <MapView
             style={style.map}
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,15 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
 import { SafeAreaView } from 'react-navigation';
 import Map from '../components/Map';
+import * as Permissions from 'expo-permissions';
+import { watchPositionAsync, Accuracy } from 'expo-location';
+import ErrorAlert from '../components/ErrorAlert';
+import '../_mockLocation';
 
 const TrackCreateScreen = () =>{
+
+    const initialLatitude = 37.33233;
+    const initialLongitude = -122.03121;
+
+    const [points, setPoints] = useState([{latitude : initialLatitude, longitude : initialLongitude}]);
+
+
+    const startWatching = async () => {
+        try{
+            
+            const response = await Permissions.askAsync(Permissions.LOCATION);
+            if(response.status !== 'granted')
+                ErrorAlert('Location tracking was not allowed.');
+
+            await watchPositionAsync({
+                accuracy : Accuracy.BestForNavigation,
+                timeInterval : 1000,
+                distanceInterval : 10
+            }, (location) => {
+               //console.log(location);
+               setPoints([...points, {latitude : location.coords.latitude, longitude : location.coords.longitude}]);
+               
+            });
+
+        } catch (err) {
+            ErrorAlert(err.message, 'Location tracking problem');
+        }
+    }
+
+    useEffect(() => {
+        startWatching();
+    }, []);
 
     return(
         <SafeAreaView forceInset={{top : 'always'}}>
             <Text h3>Create a track</Text>
-            <Map />
+            <Map
+                initialLatitude={initialLatitude}
+                initialLongitude={initialLongitude}
+                points={points}
+            />
         </SafeAreaView>
     )
 }
