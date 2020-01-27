dc94ea849de20cc87cc431ac398c37a8b13e79ea
Michel <contact@michelcurti.dev>
2020-01-27 16:24:52 -0300

    Tracks context management

--- src/context/tracksContext.js
+++ src/context/tracksContext.js
@@ -6,11 +6,28 @@ const tracksReducer = (state, action) => {
     
     switch(action.type){
 
-        case 'setTrack':
+        case 'addTrack':
             return [
                 ...state,
                 action.payload
             ];
+
+        case 'removeTrack':
+            console.log(action.payload);
+            return state.filter(track => track._id !== action.payload);
+
+        case 'fetchTracks':
+            return action.payload;
+
+        case 'getTrack':
+            return state.map(track => {
+                if(track._id === action.payload._id)
+                    return action.payload;
+
+                else
+                    return track;
+
+            });
 
         default:
             return state
@@ -20,10 +37,77 @@ const tracksReducer = (state, action) => {
 
 const actions = {
 
-    setTrack : dispatch => async (name, locations) => {
+    addTrack : dispatch => async (name, locations) => {
 
-        const response = await trackerAPI.post('/tracks', {name, locations});
+        try{
+            const response = await trackerAPI.post('/tracks', {name, locations});
+            
+            dispatch({
+                type : 'addTrack',
+                payload : {
+                    name,
+                    locations
+                }
+            });
 
+        }catch(err){
+            console.log("ERROR => " + err.response.data);
+            ErrorAlert(err.response.data.error, "Could't add track");
+        }
+        
+    },
+
+    removeTrack : dispatch => async(id) => {
+
+        try{
+            const response = await trackerAPI.delete(`/tracks/${id}`);
+            
+            dispatch({
+                type : 'removeTrack',
+                payload : response.data._id
+            });
+            
+        }catch(err){
+            let errorMessage = "";
+
+            if(err.message)
+                errorMessage = err.message;
+
+            else
+                errorMessage = err.response.data.error;
+
+            ErrorAlert(errorMessage, "Couldn't remove track");
+        }
+    },
+
+    fetchTracks : dispatch => async() => {
+        
+        try{
+            const response = await trackerAPI.get(`/tracks`);
+            
+            //console.log(response.data);
+            dispatch({
+                type : 'fetchTracks',
+                payload : response.data
+            });
+            
+        }catch(err){
+            ErrorAlert(err.response.data.error, "There was a problem");
+        }
+    },
+
+    getTrack : dispatch => async (id) => {
+
+        try{
+            const response = await trackerAPI.get(`/tracks/:${id}`);
+            dispatch({
+                type : 'getTrack',
+                payload : response.data
+            });
+
+        }catch(err){
+            ErrorAlert(err.response.data.error, "There was a problem");
+        }
     }
 
 };
