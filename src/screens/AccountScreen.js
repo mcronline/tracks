1c2cbdf1a73867f448905673d78731a8e3a3c420
Michel <contact@michelcurti.dev>
2020-01-27 16:23:42 -0300

    Giving some space fot content to be out the status bar

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -2,16 +2,19 @@ import React, { useContext } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { Button } from 'react-native-elements';
 import { Context as authContext} from '../context/authContext';
+import { SafeAreaView } from "react-navigation";
 
 const AccountScreen = () =>{
 
     const { signout } = useContext(authContext);
 
     return(
-        <View>
-            <Text style={{fontSize : 48}}>AccountScreen</Text>
-            <Button title="Sign Out" onPress={signout} />
-        </View>
+        <SafeAreaView forceInset={{ top : 'always'}}>
+            <View>
+                <Text style={{fontSize : 48}}>AccountScreen</Text>
+                <Button title="Sign Out" onPress={signout} />
+            </View>
+        </SafeAreaView>
     )
 }
 
